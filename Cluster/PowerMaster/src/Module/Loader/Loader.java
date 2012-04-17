993fb252662974fa7341735e37288d865a45de5a
Bruno Oliveira <bmfo.dev@gmail.com>
2012-04-18 00:39:19 +0100

    [PowerMaster] Implementação de um novo loader para o problema KnapSack. Está implementação não esta oficializada, uma vez que pode e deverá estar sujeita a alterações.
    A documentação deste loader ainda não foi feita devido a tratar-se de uma versão muito prematura.

--- Cluster/PowerMaster/src/Module/Loader/Loader.java
+++ Cluster/PowerMaster/src/Module/Loader/Loader.java
@@ -18,7 +18,8 @@ public class Loader {
         //Leitura da primeira linha para verificar qual o tipo de problema
         String[] primeiraLinha = linhas[0].split(";");
         //Verificação do tipo de problema (Linha 1)
-        if (primeiraLinha[0].equals("OneMax")) {
+        if (primeiraLinha[0].equals(OnesMax.ProblemName)) {
+            System.out.println("##### New Ones Max Problem #####");
             //Problema do tipo OnesMax
             OnesMax oneMax = new OnesMax(linhas);
             //Verificar se o objecto foi carregado com sucesso com a informação passada
@@ -26,9 +27,26 @@ public class Loader {
                 //Atribnuição de Identificadores globais
                 oneMax.setProblemID(Integer.parseInt(primeiraLinha[1]));
                 oneMax.setClientID(Integer.parseInt(primeiraLinha[2]));
+                System.out.println("##### OnesMax Problem Ok #####");
                 return (Problem)oneMax;
             }
+            System.out.println("##### OnesMax NOT LOADED #####");
         }
+        if (primeiraLinha[0].equals(KnapSack.ProblemName)) {
+            System.out.println("##### New KnapSack Problem #####");
+            //Problema do tipo OnesMax
+            KnapSack knapSack = new KnapSack(linhas);
+            //Verificar se o objecto foi carregado com sucesso com a informação passada
+            if(knapSack.getStatus()){
+                //Atribnuição de Identificadores globais
+                knapSack.setProblemID(Integer.parseInt(primeiraLinha[1]));
+                knapSack.setClientID(Integer.parseInt(primeiraLinha[2]));
+                System.out.println("##### KnapSack Problem Ok #####");
+                return (Problem)knapSack;
+            }
+            System.out.println("##### KnapSack NOT LOADED #####");
+        }        
+        
         return null;
     }
 }
