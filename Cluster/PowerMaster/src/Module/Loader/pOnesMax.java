e3dbdf11ea28162259deb894b2235542e2da1f9f
Bruno Oliveira <bmfo.dev@gmail.com>
2012-04-24 11:26:42 +0100

    [PowerMaster] Primeira versão do loader com o JSON. O carregamento de Operadores também esta feito. Apenas está implementado OnesMax.

--- Cluster/PowerMaster/src/Module/Loader/pOnesMax.java
+++ Cluster/PowerMaster/src/Module/Loader/pOnesMax.java
@@ -11,13 +11,10 @@ import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import operators.Operator;
-import operators.mutation.Flipbit;
-import operators.recombinations.Crossover;
-import operators.replacements.Tournament;
+import org.json.JSONException;
+import org.json.JSONObject;
 import powermaster.GeneticEvents;
 import powermaster.PowerMaster;
-import utils.exceptions.SolverException;
-import utils.exceptions.SonsInicialitazionException;
 
 /**
  *
@@ -53,79 +50,65 @@ public class pOnesMax extends Problem {
     private int PARAM_ADDED = 0;//Número de parametros adicionados obrigatórios
     private static int PARAM_COUNT = 4;//Número de parametros existentes no problema
     /**
-     * Referência para todas as linhas existentes no documento
-     */
-    private String data[];
-    /**
      * Variável que indica o estado do loader
      */
     private boolean loadStatus;
+    private JSONObject data;
 
     /**
      * Construtor do problema OnesMax
      * @param data Informação do problema a ser carregado
      */
-    public pOnesMax(String data[]) {
+    public pOnesMax(JSONObject data) throws Exception {
         super(pOnesMax.ProblemName, PARAM_REQUIRED);
         this.data = data;
         this.loadStatus = Load();
     }
 
-    private boolean Load() {
-        //Ler a segunda linha que contem todos os parametros de arranque
-        String[] parmsData = data[1].split(";");
-        //Percorrer todos os parametros
-        for (int i = 0; i < parmsData.length; i++) {
-            //Separar o identificado do valor (=)
-            String[] param = parmsData[i].split("=");
-            //Verificação de parametros
-            //parametro 1
-            Boolean verInt = Problem.VerInt(param);
-
-            if (verInt) {
-                if (param[0].equals(pOnesMax.PARAM_ITERATIONS) && !this.containsParam(pOnesMax.PARAM_ITERATIONS)) {
-                    //carregar o parametro para um dicionario de parametros
-                    this.Iterations = Integer.parseInt(param[1]);
-                    this.addParam(pOnesMax.PARAM_ITERATIONS, this.Iterations);
-                    //registar parametros obrigatório
-                    this.PARAM_ADDED++;
-                    System.out.println(pOnesMax.PARAM_ITERATIONS + "+:" + this.Iterations);
-                    continue;
-                }
-                //parametro 2
-                if (param[0].equals(pOnesMax.PARAM_POPULATION_SIZE) && !this.containsParam(pOnesMax.PARAM_POPULATION_SIZE)) {
-                    this.popSize = Integer.parseInt(param[1]);
-                    this.addParam(pOnesMax.PARAM_POPULATION_SIZE, this.popSize);
-                    //registar parametros obrigatório
-                    this.PARAM_ADDED++;
-                    System.out.println(pOnesMax.PARAM_POPULATION_SIZE + "+:" + this.popSize);
-                    continue;
-                }
-                //parametro 3
-                if (param[0].equals(pOnesMax.PARAM_ALELLO_SIZE) && !this.containsParam(pOnesMax.PARAM_ALELLO_SIZE)) {
-                    this.alelloSize = Integer.parseInt(param[1]);
-                    this.addParam(pOnesMax.PARAM_ALELLO_SIZE, this.alelloSize);
-                    //registar parametros obrigatório
-                    this.PARAM_ADDED++;
-                    System.out.println(pOnesMax.PARAM_ALELLO_SIZE + "+:" + this.alelloSize);
-                    continue;
-                }
-                //parametro 4
-                if (param[0].equals(pOnesMax.PARAM_BEST_FITNESS) && !this.containsParam(pOnesMax.PARAM_BEST_FITNESS)) {
-                    this.bestFitness = Integer.parseInt(param[1]);
-                    this.addParam(pOnesMax.PARAM_BEST_FITNESS, this.bestFitness);
-                    //registar parametros obrigatório
-                    this.PARAM_ADDED++;
-                    System.out.println(pOnesMax.PARAM_ALELLO_SIZE + "+:" + this.bestFitness);
-                    continue;
-                }
-            } else {
-                return false;
+    private boolean Load() throws Exception {
+        try {
+            if (!this.containsParam(pOnesMax.PARAM_ITERATIONS)) {
+                //carregar o parametro para um dicionario de parametros
+                this.Iterations = this.data.getInt(pOnesMax.PARAM_ITERATIONS);
+                this.addParam(pOnesMax.PARAM_ITERATIONS, this.Iterations);
+                //registar parametros obrigatório
+                this.PARAM_ADDED++;
+                System.out.println(pOnesMax.PARAM_ITERATIONS + "+:" + this.Iterations);
             }
+            //parametro 2
+            if (!this.containsParam(pOnesMax.PARAM_POPULATION_SIZE)) {
+                this.popSize = this.data.getInt(pOnesMax.PARAM_POPULATION_SIZE);
+                this.addParam(pOnesMax.PARAM_POPULATION_SIZE, this.popSize);
+                //registar parametros obrigatório
+                this.PARAM_ADDED++;
+                System.out.println(pOnesMax.PARAM_POPULATION_SIZE + "+:" + this.popSize);
+            }
+            //parametro 3
+            if (!this.containsParam(pOnesMax.PARAM_ALELLO_SIZE)) {
+                this.alelloSize = this.data.getInt(pOnesMax.PARAM_ALELLO_SIZE);
+                this.addParam(pOnesMax.PARAM_ALELLO_SIZE, this.alelloSize);
+                //registar parametros obrigatório
+                this.PARAM_ADDED++;
+                System.out.println(pOnesMax.PARAM_ALELLO_SIZE + "+:" + this.alelloSize);
+            }
+            //parametro 4
+            if (!this.containsParam(pOnesMax.PARAM_BEST_FITNESS)) {
+                this.bestFitness = this.data.getInt(pOnesMax.PARAM_BEST_FITNESS);
+                this.addParam(pOnesMax.PARAM_BEST_FITNESS, this.bestFitness);
+                //registar parametros obrigatório
+                this.PARAM_ADDED++;
+                System.out.println(pOnesMax.PARAM_ALELLO_SIZE + "+:" + this.bestFitness);
+            }
+        } catch (Exception e) {
+            this.loadStatus=false;
+            e.printStackTrace();
+            throw e;
         }
+
 
         //Verificar se todos os parametros foram carregados
         if (this.PARAM_ADDED == pOnesMax.PARAM_REQUIRED) {
+            this.loadStatus=true;
             //Ler dados (restantes Linhas)
             //Neste caso não existe a necessidade de carregar parametros
 
@@ -147,16 +130,23 @@ public class pOnesMax extends Problem {
      */
     @Override
     public Solver getNewSolver() {
+        
+            //        // Operadores
+            //        ArrayList<Operator> __operators = new ArrayList<Operator>();
+            //        //__operators.add(new SUS(70)); 
+            //        __operators.add(new operators.selections.Tournament(70, 2));
+            //        __operators.add(new operators.recombinations.Crossover());
+            //        __operators.add(new operators.mutation.Flipbit(0.01));
+            //        __operators.add(new operators.replacements.Tournament(100, 2));
 
-        // Operadores
-        ArrayList<Operator> __operators = new ArrayList<Operator>();
-        //__operators.add(new SUS(70)); 
-        __operators.add(new operators.selections.Tournament(70, 2));       
-        __operators.add(new operators.recombinations.Crossover());
-        __operators.add(new operators.mutation.Flipbit(0.01));
-        __operators.add(new operators.replacements.Tournament(100, 2));
-
-        int __sizePopulation = this.popSize; 
+        try {
+             this.LoadOperators(data);
+        } catch (JSONException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        
+        int __sizePopulation = this.popSize;
         int __sizeAllelo = this.alelloSize;
         Individual __prototypeIndividual = new genetics.algorithms.OnesMax();
         int __iteractions = this.Iterations;
@@ -172,10 +162,10 @@ public class pOnesMax extends Problem {
 
         StopCriterion __stopCriterion = new StopCriterion(__iteractions, __bestFitness);
 
-        Solver solver = new Solver(__sizePopulation,__sizeAllelo,__prototypeIndividual,__stopCriterion,__operators,new GeneticEvents(PowerMaster.INTERVAL_PART,1,1));
+        Solver solver = new Solver(__sizePopulation, __sizeAllelo, __prototypeIndividual, __stopCriterion, this.getOperators(), new GeneticEvents(PowerMaster.INTERVAL_PART, 1, 1));
         return solver;
         //return new Solver(__sizePopulation, __sizeAllelo, __prototypeIndividual,__stopCriterion,__operators, new GeneticEvents(PowerMaster.INTERVAL_PART,1,1));
-    
+
     }
 
     public boolean getStatus() {
