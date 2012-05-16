71a95aa701d3230dd79d6e925a4124803566698d
Bruno Oliveira <bmfo.dev@gmail.com>
2012-05-16 15:33:05 +0100

    [PowerMaster] V0.3

---
+++ Cluster/PowerMaster/src/powermaster/SaveStatus.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package powermaster;
+
+import genetics.Population;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Bruno Oliveira nº 11127 IPT-ESTT
+ */
+public class SaveStatus implements Serializable{
+    
+    private String Name;
+    private ArrayList<Population> pops;
+    
+    public SaveStatus(String Name){
+        this.Name = Name;
+        pops = new ArrayList<Population>();
+    }
+    
+    public void AddPopulation(Population pop){
+        pops.add(pop);
+    }
+    
+}
