6a59c07051d3f65c0c447096388bf6dcd3ded3fe
Filipe Cardoso <filufo@hotmail.com>
2012-07-05 23:28:24 +0100

    Versão final

---
+++ PowerMaster_Site/Conexao_normal/src/conexao_normal/Conexao_normal.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexao_normal;
+
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+import java.applet.*;
+import java.awt.Graphics;
+
+/**
+ *
+ * @author filipe
+ */
+public class Conexao_normal extends Applet {
+
+    /**
+     * @param args the command line arguments
+     */
+    public void paint(Graphics g){
+        try {
+            Socket socket = new Socket("code.dei.estt.ipt.pt", 10001);
+            Scanner in = new Scanner(socket.getInputStream());
+            PrintStream out = new PrintStream(socket.getOutputStream());
+            out.println("processos");
+            g.drawString("Pedido enviado! Por favor aguarde!", 100, 20);
+            String msg = in.nextLine();
+
+
+            g.drawString("Received: " + msg, 100, 40);
+            socket.close();
+            in.close();
+            out.close();
+        } catch (Exception e) {
+            g.drawString("Impossivel estabelecer ligação com o servidor!" + e, 0, 20);
+        }
+    }
+}
---
+++ PowerMaster_Site/teste/src/teste/Teste.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teste;
+import java.applet.*;
+import java.awt.Graphics;
+
+/**
+ *
+ * @author filipe
+ */
+public class Teste extends Applet {
+
+    /**
+     * @param args the command line arguments
+     */
+    public void paint(Graphics g){
+        g.drawString("Olá", 100, 20);
+    }
+}
